1d3f93276685eb9895688aea92e58319098a7e1c
Vilas Patil <70201080+patildipak422@users.noreply.github.com>
2020-09-16 09:04:58 -0700

    Adding two Numbers using function call

---
+++ Java Practice/AdditionUsingClass.java
@@ -0,0 +1,10 @@
+class AdditionUsingMethod {
+	public static void main(String[] args){
+		add();
+	}
+	public static void add(){
+		int num1=20;
+		int num2=200;
+		System.out.print(num1+num2);
+	}
+}
