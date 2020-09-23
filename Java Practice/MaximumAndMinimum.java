121bb646fd9a1e8193ea4ae129c84954e3f50c2f
Vilas Patil <70201080+patildipak422@users.noreply.github.com>
2020-09-23 15:29:33 +0530

    Finding Maximum and Minimum elements using method call

--- Java Practice/MaximumAndMinimum.java
+++ Java Practice/MaximumAndMinimum.java
@@ -1,8 +1,28 @@
 import java.util.Scanner;
 class MaximumAndMinimum{
+    public void maximum(int [] array, int size){
+        int max;
+        max=array[0];
+        for(int k=0; k<size; k++){
+            if(array[k]>max){
+                max=array[k];
+            }
+        }
+        System.out.println("The Maximum Element is " + max);
+    }
+    public void minimum(int [] array, int size){
+        int min;
+        min=array[0];
+        for(int l=0; l<size; l++){
+            if(array[l]<min){
+                min=array[l];
+            }
+        }
+        System.out.print("The Maximum Element is " + min);
+    }
     public static void main(String[] args){
         int size;
-        int max;
+        int min;
         Scanner in=new Scanner(System.in);
         System.out.print("Enter the Number of Elements : ");
         size= in.nextInt();
@@ -11,12 +31,9 @@ class MaximumAndMinimum{
             System.out.print("Enter the " + i + " Element :");
             array[i]= in.nextInt();
         }
-        max=array[0];
-        for(int k=0; k<size; k++){
-            if(array[k]>max){
-                max=array[k];
-            }
-        }
-        System.out.print("The Maximum Element is " + max);
+        MaximumAndMinimum maxi = new MaximumAndMinimum();
+        maxi.maximum(array, size);
+        MaximumAndMinimum mini = new MaximumAndMinimum();
+        mini.minimum(array, size);
     }
 }
