a4b918fefffb06abc4159f9107aa943358f2b308
Vilas Patil <70201080+patildipak422@users.noreply.github.com>
2020-09-26 00:35:07 +0530

    updated method call and included objects

--- Java Practice/FindAElement.java
+++ Java Practice/FindAElement.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
 public class FindAElement {
+    public int search(int [] array, int num){
+        int ctr=0;
+        for(int k : array){
+            if(k==num){
+                 ctr++;
+                 break;
+             }
+        }
+        return ctr;
+    }
+    public void print(int ctr, int num){
+        if(ctr==0){
+            System.out.print("The element " + num + " is not Present");
+        }
+        else{
+            System.out.print("The element " + num + " is Present");
+        }
+    }
     public static void main(String[] args) {
         int size;
         int num;
-        int ctr=0;
-        Scanner in = new Scanner(System.in);
+        int ctr;
         System.out.print("Enter the Number of elements : ");
+        Scanner in = new Scanner(System.in);
         size=in.nextInt();
         int array[]= new int[size];
         for(int i=0; i<size; i++){
@@ -14,10 +32,9 @@ public class FindAElement {
         }
         System.out.print("Enter the Number to be find : ");
         num=in.nextInt();
-        for(int k : array){
-           if(k==num){
-               System.out.print("The element " + num + " is Present");
-           }
-        }
+        FindAElement obj = new FindAElement();
+        ctr=obj.search(array, num);
+        FindAElement obj1 = new FindAElement();
+        obj1.print(ctr, num);
     }
 }
