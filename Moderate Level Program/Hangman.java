05b446cba37a4a462ba367a049eb9047a58fca68
Vilas Patil <patildipak422@gmail.com>
2020-09-28 16:07:17 +0530

    Added Comments and Taking input from user

--- Moderate Level Program/Hangman.java
+++ Moderate Level Program/Hangman.java
@@ -5,17 +5,19 @@ public class Hangman {
     public static void main(String[] args) {
         String str = "elephant";
         char guess;
+        int ctr=0;
         int chances=str.length();
+        Scanner in = new Scanner(System.in);
+        char word [] = str.toCharArray();                           //Array to Store the Original Word or Question word
+        char guesses [] = new char [100];                           //Array to Store the User Gusses
+        char letters [] = new char [str.length()];                  //Array to Show after the user guesses
+        for(int i=0; i<str.length(); i++){
+            letters[i]= '-';
+        }
+        System.out.println("Welcome to the Game");
         do{
-            Scanner in = new Scanner(System.in);
-            char word [] = str.toCharArray();
-            char letters [] = new char [str.length()];
-            char guesses [] = new char [100];
-            System.out.print("Welcome to the Game");
-            System.out.println();
             System.out.print("Word : ");
             for(int i=0; i<str.length(); i++){
-                letters[i]= '-';
                 System.out.print(letters[i]);
             }
             System.out.println();
@@ -26,6 +28,18 @@ public class Hangman {
                     letters[i]= guess;
                 }
             }
+            for (int k=0; k<str.length(); k++) {
+                System.out.print(letters[k] + " ");
+            }
+            for(int i=0; i<str.length(); i++){
+                if(word[i]==letters[i]);
+                ctr++;
+            }
+            if(ctr==9){
+                System.out.println("Congratulation You are a Winner !!");
+            }
+            // tried to print that u are a winner but not completed
+            System.out.println();
             chances--;
         }while(chances!=0);
     }
