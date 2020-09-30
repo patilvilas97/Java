d4228d079ce115b9575923585edddec892aae886
Vilas Patil <patildipak422@gmail.com>
2020-09-30 22:55:10 +0530

    Initial Commit

---
+++ Moderate Level Program/InsertingInAArray.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class InsertingInAArray{
+    public static void main(String[] args) {
+        int size;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Size");
+        sixe=in.nextInt();
+        int array= new int [size];
+    }
+}
