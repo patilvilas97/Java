910e3c8de79818c9ea3eec2c953283a0ceed8750
Vilas Patil <70201080+patildipak422@users.noreply.github.com>
2020-10-03 22:17:33 +0530

    Initial Commit

---
+++ Java Practice/EqualityOfArray.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+Class EqualityOfArray{
+    public static void main(){
+        Scanner in = new Scanner(System.in);
+        int array1 [] = new int [5];
+        int array2 [] = new int [5];
+        for(int i=0; i<5; i++){
+            array1[i] = in.nextInt();
+        }
+        for(int i=0; i<5; i++){
+            array2[i] = in.nextInt();
+        }
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                if()
+            }
+        }
+    }
+}
