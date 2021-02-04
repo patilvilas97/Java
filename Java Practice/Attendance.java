059feb28f8544b1ffecfb136a1e9a70ec8760eac
Vilas Patil <patildipak422@gmail.com>
2021-02-04 13:04:06 +0530

    adding new arrays

--- Java Practice/Attendance.java
+++ Java Practice/Attendance.java
@@ -7,7 +7,7 @@ However, whenever there are multiple students with the same first name, Chef has
 of all these students. For each student that does not share the first name with any other student, Chef may still call out only this 
 student's first name. Help Chef decide, for each student, whether he will call out this student's full name or only the first name.*/
 
-java.util.Scanner;
+import java.util.Scanner;
 public class Attendance {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -17,13 +17,26 @@ public class Attendance {
         testcases = in.nextInt();
         System.out.print("Please Enter the Number of Students : ");
         number_of_students = in.nextInt();
+        String [] first_name = new String[number_of_students];
+        String [] last_name = new String[number_of_students];
         String [] students = new String[number_of_students];
         for(int i=0; i<number_of_students; i++){
             System.out.print("Please Enter the Name of  Student : ");
-            students[i] = in.next();
+            first_name[i] = in.next();
+            last_name[i] = in.next();
         }
         for(int j=0; j<number_of_students; j++){
-            if(students)
+            for(int k=0; k<number_of_students; k++){
+                if(first_name[j] == first_name[k]){
+                    students[k] = first_name[k] + last_name[k];
+                }
+                else{
+                    students[j] = first_name[j];
+                }
+            }
+        }
+        for (String l : students) {
+            System.out.println(l);
         }
     }
 }
