fae08c8f1f017e264525a89257fa6ec2238ed627
Vilas Patil <patildipak422@gmail.com>
2021-02-25 23:53:07 +0530

    Initial Commit

---
+++ Moderate Level Program/ReturnSecondLargestElement.java
@@ -0,0 +1,11 @@
+/*The function accepts an integers arr of size ’length’ as its arguments you are required to return the sum of second largest 
+largest element from the even positions and second smallest from the odd position of given ‘arr’*/
+import java.util.Scanner;
+public class ReturnSecondLargestElement{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please Enter the size of array : ");
+        int size = in.nextInt();
+        int arr [] = new int [size];
+    }
+}
