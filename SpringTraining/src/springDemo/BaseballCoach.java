346f2992f572497696ebfd6beae615a99dc1640e
Vilas Patil <patildipak422@gmail.com>
2022-03-27 11:03:11 +0530

    added the coach

--- SpringTraining/src/springDemo/BaseballCoach.java
+++ SpringTraining/src/springDemo/BaseballCoach.java
@@ -1,7 +1,8 @@
 package springDemo;
 
-public class BaseballCoach {
-
+public class BaseballCoach implements Coach{
+	
+	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
 	}
