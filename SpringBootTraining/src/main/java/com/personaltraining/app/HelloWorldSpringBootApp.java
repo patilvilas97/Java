ddb1cdda1bf0cd010088379fcda285b2a9cc0c0d
Vilas Patil <patildipak422@gmail.com>
2022-10-28 15:44:07 +0530

    initial commit

---
+++ SpringBootTraining/src/main/java/com/personaltraining/app/HelloWorldSpringBootApp.java
@@ -0,0 +1,11 @@
+package com.personaltraining.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootApp {
+
+	public static void main() {
+		
+	}
+}
