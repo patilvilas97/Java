cb0c9cbf11742e848fecdf60fe7878647bec9357
Vilas Patil <patildipak422@gmail.com>
2022-10-30 20:06:43 +0530

    Added Getters and Setters

--- SpringBoot/TicketBookingManagementApp/src/main/java/com/training/book/ticket/app/entities/Ticket.java
+++ SpringBoot/TicketBookingManagementApp/src/main/java/com/training/book/ticket/app/entities/Ticket.java
@@ -33,5 +33,44 @@ public class Ticket {
 	@Column(name="email")
 	private String email;
 	
+	//Adding the required Getters and Setters
+	
+	public Integer getTicketId() {
+		return ticketId;
+	}
+	public void setTicketId(Integer ticketId) {
+		this.ticketId = ticketId;
+	}
+	
+	public String getPassengerName() {
+		return passengerName;
+	}
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+	public Date getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(Date bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+	public String getSourceStaion() {
+		return sourceStation;
+	}
+	public void setSourceStation(String sourceStation) {
+		this.sourceStation = sourceStation;
+	}
+	public String getDestStaion() {
+		return destStation;
+	}
+	public void setDestStation(String destStation) {
+		this.destStation = destStation;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
