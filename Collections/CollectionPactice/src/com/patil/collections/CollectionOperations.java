0f1e595445a4707022144a9c0aef8cf1d0c06d78
Vilas Patil <patildipak422@gmail.com>
2022-12-08 13:09:03 +0530

    added arraylist

--- Collections/CollectionPactice/src/com/patil/collections/CollectionOperations.java
+++ Collections/CollectionPactice/src/com/patil/collections/CollectionOperations.java
@@ -1,7 +1,16 @@
 package com.patil.collections;
 
+import java.util.*;
+
 public class CollectionOperations {
 	public static void main(String[] args) {
+		
+		ArrayList<Integer> historyMarks = new ArrayList<Integer>();
+		historyMarks.add(97);
+		historyMarks.add(98);
+		historyMarks.add(52);
+		historyMarks.add(88);
+		System.out.println("History Marks: "+historyMarks.get(3));
 		
 	}
 
